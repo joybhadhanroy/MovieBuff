cf7e609e02113747082221dfb87eb33af29d4633
joybhadhanroy <joy.bhadhan.roy@gmail.com>
2018-04-01 14:30:59 -0500

    commit

---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using MovieBuff.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieBuff.Dtos
+{
+    public class CustomerDto
+    {
+        // fluent api for annotation
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+
+        //[Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
